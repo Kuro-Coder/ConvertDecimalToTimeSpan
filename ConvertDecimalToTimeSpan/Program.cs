03a8dd8a494d9d8facb11b14e174d6a1ecf184db
Habib PourKhnLar Ahmadi <habib.pa98@gmail.com>
2023-10-17 12:56:18 +0330

    show time with day

--- ConvertDecimalToTimeSpan/Program.cs
+++ ConvertDecimalToTimeSpan/Program.cs
@@ -7,6 +7,13 @@ Console.WriteLine($"WorkTime is: {workTime}");
 decimal repetitions = 220;
 Console.WriteLine($"Repetitions Work is: {repetitions}");
 
+var totalWorkTime = workTime.Minutes * repetitions;
+Console.WriteLine($"Total Work Time is: {totalWorkTime}");
 
+var totalWorkHours = String.Format("{0:0.00}", (totalWorkTime / 60));
+Console.WriteLine($"Total Work Hours is: {totalWorkHours}");
+
+TimeSpan timespan = TimeSpan.FromHours(double.Parse(totalWorkHours));
+Console.WriteLine($"timespan is: {timespan}"); //{3.01:19:48} show day(3)
 
 Console.ReadKey();

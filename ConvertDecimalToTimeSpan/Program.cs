f1effbfc68314abdd81918c0bf71d641fed0e9f2
Habib PourKhnLar Ahmadi <habib.pa98@gmail.com>
2023-10-17 12:43:08 +0330

    Add project files.

---
+++ ConvertDecimalToTimeSpan/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!. I'm KuroCoder.");
+
+var workTime = TimeSpan.Parse("00:20:00");
+Console.WriteLine($"WorkTime is: {workTime}");
+
+decimal repetitions = 220;
+Console.WriteLine($"Repetitions Work is: {repetitions}");
+
+
+
+Console.ReadKey();

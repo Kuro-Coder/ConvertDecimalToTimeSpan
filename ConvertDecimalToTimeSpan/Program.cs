da45e43d0f8b32db2ed8261fac719a1b5e2bc365
Habib PourKhnLar Ahmadi <habib.pa98@gmail.com>
2023-10-17 13:30:13 +0330

    show full Hours time span like ==> 73:19:48

--- ConvertDecimalToTimeSpan/Program.cs
+++ ConvertDecimalToTimeSpan/Program.cs
@@ -2,18 +2,24 @@
 Console.WriteLine("Hello, World!. I'm KuroCoder.");
 
 var workTime = TimeSpan.Parse("00:20:00");
-Console.WriteLine($"WorkTime is: {workTime}");
+Console.WriteLine($"WorkTime is: {workTime}"); // 00:20:00
 
 decimal repetitions = 220;
-Console.WriteLine($"Repetitions Work is: {repetitions}");
+Console.WriteLine($"Repetitions Work is: {repetitions}"); // 220
 
 var totalWorkTime = workTime.Minutes * repetitions;
-Console.WriteLine($"Total Work Time is: {totalWorkTime}");
+Console.WriteLine($"Total Work Time is: {totalWorkTime}"); // 4400
 
 var totalWorkHours = String.Format("{0:0.00}", (totalWorkTime / 60));
-Console.WriteLine($"Total Work Hours is: {totalWorkHours}");
+Console.WriteLine($"Total Work Hours is: {totalWorkHours}"); // 73/33
+
+//TimeSpan timespan = TimeSpan.FromHours(double.Parse(totalWorkHours));
+//Console.WriteLine($"timespan is: {timespan}"); //{3.01:19:48} show with day(3)
+
+//=========================================================== 
 
 TimeSpan timespan = TimeSpan.FromHours(double.Parse(totalWorkHours));
-Console.WriteLine($"timespan is: {timespan}"); //{3.01:19:48} show day(3)
+var totalWorkedTime = string.Format("{0}:{1}:{2}", timespan.Days * 24 + timespan.Hours, timespan.Minutes, timespan.Seconds);
+Console.WriteLine($"timespan is: {totalWorkedTime}"); // 73:19:48
 
 Console.ReadKey();
